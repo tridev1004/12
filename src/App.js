178a835affb2933c19952c7d493de455d8e3d217
Tribhuvan(tridev) singh <85545474+tridev1004@users.noreply.github.com>
2022-05-10 16:59:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+// import Greetings from './components/Greetings';
+import Greeting from './components/Greeting';
+function App() {
+  return (
+    <div className="App">
+    
+        <Greeting/>
+    </div>
+  );
+}
+
+export default App;
